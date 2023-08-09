0097c97d5256fc09f571ee4b98a7b577348f6d1f
Kinjo Makito <s23007@std.it-college.ac.jp>
2023-08-09 07:56:22 +0000

    test

---
+++ CHAPTER5/Q5_1_3.py
@@ -0,0 +1,9 @@
+queue = []
+queue.append(1)
+print('queue:', queue)
+queue.append(2)
+print('queue:', queue)
+print('dequeue 1st value:', queue.pop(0))
+print('dequeue 2nd value:', queue.pop(0))
+print('queue:', queue)
+
